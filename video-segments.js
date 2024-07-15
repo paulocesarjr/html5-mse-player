049ba8ed185d01911cc612fba3314f3b1f4a8098
Paulo Cesar Jr <paulocesarrosajr@gmail.com>
2024-07-15 20:19:38 -0300

    refactor: create mse_player file

---
+++ video-segments.js
@@ -0,0 +1,6 @@
+export default [
+  "https://testcontent.eyevinn.technology/mse-tutorial/vinn-video=1660000.dash",
+  "https://testcontent.eyevinn.technology/mse-tutorial/vinn-video=1660000-0.dash",
+  "https://testcontent.eyevinn.technology/mse-tutorial/vinn-video=1660000-25600.dash",
+  "https://testcontent.eyevinn.technology/mse-tutorial/vinn-video=1660000-51200.dash"
+]
