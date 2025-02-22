c948c3e4af7622e0f0bd618d3c3aab89c9ebee01
Paulo Cesar Jr <paulocesarrosajr@gmail.com>
2025-02-22 14:19:29 -0300

    refactor: change chunks url

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 import MSEVideoPlayer from "./mse_video_player"
+import videoSegments from "./video-segments"
 
 const mimeCodec = 'video/mp4; codecs="avc1.4D401F"'
-const videoSegments = [
-  "https://testcontent.eyevinn.technology/mse-tutorial/vinn-video=1660000.dash",
-  "https://testcontent.eyevinn.technology/mse-tutorial/vinn-video=1660000-0.dash",
-  "https://testcontent.eyevinn.technology/mse-tutorial/vinn-video=1660000-25600.dash",
-  "https://testcontent.eyevinn.technology/mse-tutorial/vinn-video=1660000-51200.dash"
-]
 
 document.addEventListener("DOMContentLoaded", () => {
   const player = new MSEVideoPlayer("videoElement", mimeCodec, videoSegments)
--- video-segments.js
+++ video-segments.js
@@ -1,6 +1,13 @@
 export default [
-  "https://testcontent.eyevinn.technology/mse-tutorial/vinn-video=1660000.dash",
-  "https://testcontent.eyevinn.technology/mse-tutorial/vinn-video=1660000-0.dash",
-  "https://testcontent.eyevinn.technology/mse-tutorial/vinn-video=1660000-25600.dash",
-  "https://testcontent.eyevinn.technology/mse-tutorial/vinn-video=1660000-51200.dash"
+  "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_3840x2160_12000k/bbb_30fps_3840x2160_12000k_0.m4v",
+  "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_3840x2160_12000k/bbb_30fps_3840x2160_12000k_1.m4v",
+  "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_3840x2160_12000k/bbb_30fps_3840x2160_12000k_2.m4v",
+  "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_3840x2160_12000k/bbb_30fps_3840x2160_12000k_3.m4v",
+  "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_3840x2160_12000k/bbb_30fps_3840x2160_12000k_4.m4v",
+  "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_3840x2160_12000k/bbb_30fps_3840x2160_12000k_5.m4v",
+  "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_3840x2160_12000k/bbb_30fps_3840x2160_12000k_6.m4v",
+  "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_3840x2160_12000k/bbb_30fps_3840x2160_12000k_7.m4v",
+  "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_3840x2160_12000k/bbb_30fps_3840x2160_12000k_8.m4v",
+  "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_3840x2160_12000k/bbb_30fps_3840x2160_12000k_9.m4v",
+  "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_3840x2160_12000k/bbb_30fps_3840x2160_12000k_10.m4v"
 ]
